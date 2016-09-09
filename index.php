a9d3e2b97168687bef445d20dbd7690255ba834a
Kenneth Nielsen <k.nielsen81@gmail.com>
2016-09-09 14:32:36 +0200

    Start the big renaming (CINF -> SurfCat)

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
   */
 
 include("common_functions_v2.php");
-echo(html_header($root="", $title="Data logged at CINF", $includehead="",
+echo(html_header($root="", $title="Data logged at <strike>CINF</strike> SurfCat", $includehead="",
 		 $charset="UTF-8", $width=null, $html5=true));
 
 #$g = simplexml_load_file("index.xml");

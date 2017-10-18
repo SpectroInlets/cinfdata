d8824fdfd7bc9394e35771ece5c698ff80f5b2c4
Robert Jensen <robert.jensen@fysik.dtu.dk>
2017-10-18 15:27:11 +0200

    Updated host_checker to suppert database caching

--- cinf_common/host_checker.php
+++ cinf_common/host_checker.php
@@ -1,3 +1,18 @@
+<?php
+include("../common_functions_v2.php");
+$db = std_db();
+
+$query = 'select id, time, host, port, location, purpose, attr from host_checker';
+$result  = mysql_query($query, $db);
+while ($row = mysql_fetch_array($result)){                                               
+  if (sizeof($row[6]) > 0){
+    $data[] = json_decode($row[6], True);
+  }
+  else{
+    $data[] = array("hostname" => $row[2], "location" => "<i>" . $row[4] . "</i>");
+  }
+}
+?>
 <html>
 <head>
 
@@ -26,53 +41,42 @@ border-radius: 50%;
 
 <body>
 <?php
-#exec('python snapshot.py',$output);
-#exec('python host_status.py',$output);
-#passthru('python host_status.py');
-#$file = file_get_contents('http://robertj/hosts.php');
-$file = file_get_contents('http://rasppi42/');
-$output = explode("\n",$file);
-
-#echo("<table border=1 padding=5>");
-#echo("<tr><th>&nbsp;</th><th>Hostname</th><th>Uptime</th><th>Load</th><th>Setup</th><th>Description</th><th>OS</th><th>Temp.</th><th>Model</th></tr>");
-
 echo("<table class=\"sortable-theme-finder\" data-sortable>\n");
 echo("<thead>");
-echo("<th>&nbsp;</th><th>Hostname</th><th>Uptime</th><th>Load</th><th>Setup</th><th>Description</th><th>OS</th><th>Git</th><th>Temp.</th><th>Python</th><th>Model</th>");
+echo("<th>&nbsp;</th><th>Hostname</th><th>Uptime</th><th>Load</th><th>Setup</th><th>Description</th><th>Apt</th><th>Git</th><th>Temp.</th><th>Python</th><th>Model</th>");
 echo("</thead><tbody>");
 
-for ($i=0;$i<sizeof($output)-2;$i++){
-  $row = explode('|',$output[$i]);
+for ($i=0; $i<sizeof($data); $i++){
   echo("<tr>");
-  $color = ($row[1] == 0) ? "\"#FF0000\"" : "\"#00FF00\"";
-  $value = ($row[1] == 0) ? "0" : "1";
+  $color = ($data[$i]['up_or_down'] == 0) ? "\"#FF0000\"" : "\"#00FF00\"";
+  $value = ($data[$i]['up_or_down'] == 0) ? "0" : "1";
 
-  #echo("<td class=" . $color . ">&nbsp;</td>");
   echo("<td data-value=\"" . $value . "\"><font color=" . $color . ">&#8226;</font></td>");
-  if (substr($row[0], 0, 6) == 'rasppi'){
-    $sortval = (substr($row[0], 6));
+  if (substr($data[$i]['hostname'], 0, 6) == 'rasppi'){
+    $sortval = (substr($data['hostname'], 6));
   }
   else{
     $sortval = 1000;
   }
-  echo("<td data-value=\"" . $sortval . "\">" . $row[0] . "</td>");
-  if ($row[1] == 0){
+  echo("<td data-value=\"" . $sortval . "\">" . $data[$i]['hostname'] . "</td>");
+  if ($data[$i]['up_or_down'] == false){
     echo("<td colspan=2><b>Host is down</b></td>");
   }else{
-       echo("<td>" . $row[2] . "</td><td>" . $row[3] . "</td>");
+       echo("<td>" . $data[$i]['up'] . "</td><td>" . $data[$i]['load'] . "</td>");
   }
-  echo("<td>" . $row[4] . "</td>");
-  echo("<td>" . $row[5] . "</td>");
-  echo("<td>" . $row[6] . "</td>");
-  echo("<td>" . $row[7] . "</td>");
-  echo("<td>" . $row[8] . "</td>");
-  echo("<td>" . $row[10] . "</td>");
-  if (strpos($row[9], '(') == True){
-    echo("<td>" . substr($row[9],0, strpos($row[9], '(')) . "</td>");
-    }
-    else{
-  echo("<td>" . $row[9] . "</td>");
-    }
+  echo("<td>" . $data[$i]['location'] . "</td>");
+  echo("<td>" . $data[$i]['purpose'] . "</td>");
+  echo("<td>" . $data[$i]['apt_up'] . "</td>");
+  echo("<td>" . $data[$i]['git'] . "</td>");
+  echo("<td>" . $data[$i]['host_temperature'] . "</td>");
+  echo("<td>" . $data[$i]['python_version'] . "</td>");
+  echo("<td>" . $data[$i]['model'] . "</td>");
+  #if (strpos($row[9], '(') == True){
+  #  echo("<td>" . substr($row[9],0, strpos($row[9], '(')) . "</td>");
+  #  }
+  #  else{
+  #echo("<td>" . $row[9] . "</td>");
+  #  }
   echo("</tr>\n");
 }
 echo("</tbody></table>");
